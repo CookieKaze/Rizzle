3d7ad1f92cc98e0072305c1b7f5b78dba26fdc5f
Erin Luu <luuerin@gmail.com>
2016-12-17 23:11:08 -0500

    Add form content and keyboard resign

--- rizzle/CreateRizzleViewController.swift
+++ rizzle/CreateRizzleViewController.swift
@@ -7,24 +7,58 @@
 //
 
 import UIKit
-struct Constants {
-   static let animationDuration = 0.3
-}
 
-class CreateRizzleViewController: UIViewController {
+class CreateRizzleViewController: UIViewController, UITextFieldDelegate {
+    //MARK: Properties
+    var currentPageCount = 0
+    var animationDuration = 0.3
+
+    
+    //Form Views
     @IBOutlet weak var quesitonView: UIView!
     @IBOutlet weak var imageUploadView: UIView!
     @IBOutlet weak var answerView: UIView!
     @IBOutlet weak var hintsView: UIView!
-
+    
+    //Form Fields
+    @IBOutlet weak var questionTextView: UITextView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var answerTextField: UITextField!
+    @IBOutlet weak var hint1TextField: UITextField!
+    @IBOutlet weak var hint2TextField: UITextField!
+    @IBOutlet weak var hint3TextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    @IBAction func saveRizzle(_ sender: UIBarButtonItem) {
+    override func viewDidLayoutSubviews() {
+        let lastPageCount = currentPageCount
+        currentPageCount = 0
+        animationDuration = 0
+        switch lastPageCount {
+        case 1:
+            removeViews(view: quesitonView)
+            break
+        case 2:
+            removeViews(view: quesitonView)
+            removeViews(view: imageUploadView)
+            break
+        case 3:
+            removeViews(view: quesitonView)
+            removeViews(view: imageUploadView)
+            removeViews(view: answerView)
+            break
+        default:
+            break
+        }
+        animationDuration = 0.3
     }
     
-
+    @IBAction func saveRizzle(_ sender: UIBarButtonItem) {
+        
+    }
+    
     //MARK: Form Transitions
     @IBAction func cancelBtnTapped(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
@@ -34,6 +68,7 @@ class CreateRizzleViewController: UIViewController {
         switch sender.tag {
         case 1:
             //Remove Question View
+            questionTextView.resignFirstResponder()
             removeViews(view: quesitonView)
             break
         case 2:
@@ -42,17 +77,21 @@ class CreateRizzleViewController: UIViewController {
             break
         case 3:
             //Remove Answer View
+            answerTextField.resignFirstResponder()
             removeViews(view: answerView)
             break
         default:
             break
         }
     }
-
+    
     func removeViews(view: UIView) {
-        UIView.animate(withDuration: Constants.animationDuration, delay: 0, options: .curveEaseIn, animations: {
-        view.center = CGPoint(x: view.center.x + self.view.frame.width, y: self.view.center.y + 10)
-        }, completion: {(success) in})
+        UIView.animate(withDuration: animationDuration, delay: 0, options: .curveEaseIn, animations: {
+            view.center = CGPoint(x: view.center.x - self.view.frame.width, y: self.view.center.y + 10)
+        }, completion: {(success) in
+            view.center = CGPoint(x: view.center.x - self.view.frame.width, y: self.view.center.y + 10)
+        })
+        currentPageCount += 1
     }
     
     @IBAction func backBtnPressed(_ sender: UIBarButtonItem) {
@@ -60,10 +99,15 @@ class CreateRizzleViewController: UIViewController {
         case 1:
             //Return Answer View
             restoreView(view: answerView)
+            hint3TextField.resignFirstResponder()
+            hint2TextField.resignFirstResponder()
+            hint1TextField.resignFirstResponder()
             break
         case 2:
             //Restore Image Upload View
             restoreView(view: imageUploadView)
+            answerTextField.resignFirstResponder()
+            
             break
         case 3:
             //Restore Question View
@@ -75,8 +119,26 @@ class CreateRizzleViewController: UIViewController {
     }
     
     func restoreView(view: UIView) {
-        UIView.animate(withDuration: Constants.animationDuration, delay: 0, options: .curveEaseIn, animations: {
+        UIView.animate(withDuration: animationDuration, delay: 0, options: .curveEaseIn, animations: {
             view.center = CGPoint(x: self.view.center.x, y: self.view.center.y + 10)
-        }, completion: {(success) in})
+        }, completion: {(success) in
+            view.center = CGPoint(x: self.view.center.x, y: self.view.center.y + 10)
+        })
+        currentPageCount -= 1
+        
+    }
+    
+    //MARK: Keyboard Resign
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        questionTextView.resignFirstResponder()
+        answerTextField.resignFirstResponder()
+        hint1TextField.resignFirstResponder()
+        hint2TextField.resignFirstResponder()
+        hint3TextField.resignFirstResponder()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
