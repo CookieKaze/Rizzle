3f7be1c6ce89c2f2a0a6f8657785b33cf3232726
Matt Mauro <matt.mauro@gmail.com>
2016-12-19 16:17:07 -0500

    coodination

--- rizzle/LetterBank.swift
+++ rizzle/LetterBank.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
-class letterBank: UIView, UICollectionViewDelegate, UICollectionViewDataSource {
+protocol RizzleControl {
+    func enterCellIntoAnswer(_cell: UICollectionViewCell)
+}
+
+class LetterBank: UIView, UICollectionViewDelegate, UICollectionViewDataSource {
     
     
     //
@@ -18,36 +22,42 @@ class letterBank: UIView, UICollectionViewDelegate, UICollectionViewDataSource {
     
     let letters:Array<String> = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
     let numbers:Array<String> = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-    
+    let characterArray = NSMutableArray()
     let dummyStringAnswer = "spider"
     let dummyNumberAnswer = "42"
     let dummyMultipleChoice = "C"
-    var dummyRizzle:Rizzle?
+    
     let usedIndexes = NSMutableArray()
+    var delegate:RizzleControl?
+    
+    public var currentRizzle:Rizzle?
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        
+        // Setting up Dummy Rizzle for testing
+        
         let random = arc4random_uniform(3)
         switch random {
         case 0:
-            let hint:Array<String> = ["ask about his tattoo", "Red & Blue suit", "Spins a web, any size"]
-            self.dummyRizzle = Rizzle.init(title: "Superhero", question: "who is Matt's favourite hero?", answer: dummyStringAnswer, creator: "Matt", hints: hint)
-            self.dummyRizzle?.type = answerType.stringAnswer
+            let hint:Array<String> = ["ask about his tattoo", "Red & Blue suit", "Spins a web, any size..."]
+            self.currentRizzle = Rizzle.init(title: "Superhero", question: "who is Matt's favourite hero?", answer: dummyStringAnswer, creator: "Matt", hints: hint)
+            self.currentRizzle?.type = answerType.stringAnswer
             break
         case 1:
             let hint:Array<String> = ["Jackie Robinson", "Hitchhiker's Guide to the Galaxy", "6 x 7 = ?"]
-            self.dummyRizzle = Rizzle.init(title: "Pop Culture", question: "What is the answer to the Meaning of Life?", answer: dummyNumberAnswer, creator: "Matt", hints: hint)
-            self.dummyRizzle?.type = answerType.numberAnswer
+            self.currentRizzle = Rizzle.init(title: "Pop Culture", question: "What is the answer to the Meaning of Life?", answer: dummyNumberAnswer, creator: "Matt", hints: hint)
+            self.currentRizzle?.type = answerType.numberAnswer
             break
         case 2:
-            let hint:Array<String> = ["ask about his tattoo", "Red & Blue suit", "Spins a web, any size"]
-            self.dummyRizzle = Rizzle.init(title: "Multiple Guess", question: "What is Erin's favourite COLOUR!? \n a) Black as Matt's soul \n b) Magenta \n c) PINK \n d) Baby Blue", answer: dummyMultipleChoice, creator: "Matt", hints: hint)
-            self.dummyRizzle?.type = answerType.multipleChoice
+            let hint:Array<String> = ["... Panther", "Delirium Tremens", "Red + White"]
+            self.currentRizzle = Rizzle.init(title: "Multiple Guess", question: "What is Erin's favourite COLOUR!? \n a) Black as Matt's soul \n b) Magenta \n c) PINK \n d) Baby Blue", answer: dummyMultipleChoice, creator: "Matt", hints: hint)
+            self.currentRizzle?.type = answerType.multipleChoice
             break
         default:
             let hint:Array<String> = ["Metallica Song", "Zombieland Intro", "Canadian phone company"]
-            self.dummyRizzle = Rizzle.init(title: "Music", question: "For Whom the ____ tolls", answer: "Bell", creator: "Matt", hints: hint)
-            self.dummyRizzle?.type = answerType.stringAnswer
+            self.currentRizzle = Rizzle.init(title: "Music", question: "For Whom the ____ tolls", answer: "Bell", creator: "Matt", hints: hint)
+            self.currentRizzle?.type = answerType.stringAnswer
             break
         }
     }
@@ -56,9 +66,9 @@ class letterBank: UIView, UICollectionViewDelegate, UICollectionViewDataSource {
     //cellConfiguration method is on LetterCell class
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         var rows:Int = 0
-        switch self.dummyRizzle?.type?.rawValue {
+        switch self.currentRizzle?.type?.rawValue {
         case 0?:
-            rows = 4
+            rows = 1
             break
         case 1?:
             rows = 1
@@ -72,26 +82,34 @@ class letterBank: UIView, UICollectionViewDelegate, UICollectionViewDataSource {
         return rows
     }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        let more = (self.dummyRizzle?.answer?.characters.count)!
-        return (self.dummyRizzle?.answer?.characters.count)!+more
+        let more = (self.currentRizzle?.answer?.characters.count)!
+        return (self.currentRizzle?.answer?.characters.count)!+more
     }
     
-    // I seriously hope this works
+    
+    // MARK: - Select Cell 
+    // select method. Should send cell to delegate (Rizzle viewController) to hold in answer array
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.delegate?.enterCellIntoAnswer(_cell: characterArray[indexPath.row] as! LetterCell)
+        characterArray.removeObject(at: indexPath.row)
+    }
+    
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let answerString = self.dummyRizzle?.answer!.uppercased()
+        let answerString = self.currentRizzle?.answer!.uppercased()
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! LetterCell
-        let characterArray = NSMutableArray()
+        
         let addedCharacters = NSMutableArray()
         
         // switch statement based on Rizzle.Type enum
-        switch self.dummyRizzle?.type?.rawValue {
+        switch self.currentRizzle?.type?.rawValue {
         case 0?:
             cell.configure(image: (self.letters[indexPath.row]))
             break
         case 1?:
             for char in (answerString?.characters)! {
-                let indC = arc4random_uniform(UInt32((self.dummyRizzle?.answer?.characters.count)! as Int))
+                let indC = arc4random_uniform(UInt32((self.currentRizzle?.answer?.characters.count)! as Int))
                 let charNumber = Int(indC)
                 if addedCharacters.contains(charNumber) == false {
                     addedCharacters.add(charNumber)
@@ -106,7 +124,7 @@ class letterBank: UIView, UICollectionViewDelegate, UICollectionViewDataSource {
             break
         case 2?:
             for char in (answerString?.characters)! {
-                let indC = arc4random_uniform(UInt32((self.dummyRizzle?.answer?.characters.count)! as Int))
+                let indC = arc4random_uniform(UInt32((self.currentRizzle?.answer?.characters.count)! as Int))
                 let charNumber = Int(indC)
                 if addedCharacters.contains(charNumber) == false {
                     addedCharacters.add(charNumber)
@@ -125,6 +143,5 @@ class letterBank: UIView, UICollectionViewDelegate, UICollectionViewDataSource {
         return cell
     }
     
-    // letterCell.swift will have function to test if userAnswer is corrent or not
 
 }
---
+++ rizzle/SolveRizzleViewController.swift
@@ -0,0 +1,124 @@
+//
+//  RizzleViewController.swift
+//  rizzle
+//
+//  Created by Matthew Mauro on 2016-12-17.
+//  Copyright © 2016 Erin Luu. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class SolveRizzleViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, RizzleControl {
+
+    @IBOutlet weak var rizzleTitle: UILabel!
+    @IBOutlet weak var rizzleDescription: UITextView!
+    @IBOutlet weak var hintsRemainingLabel: UILabel!
+    @IBOutlet weak var dismissButton: UIButton!
+    
+    @IBOutlet weak var letterBankCollection: UICollectionView!
+    @IBOutlet weak var answerCollection: UICollectionView!
+    
+    var hintNumber:NSInteger?
+    @IBOutlet weak var hintLabel: UILabel!
+    @IBOutlet weak var solvedImage: UIImageView!
+    @IBOutlet weak var bank: LetterBank!
+    public var currentRizzle:Rizzle?
+    var answerArray = NSMutableArray()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.bank.currentRizzle = self.currentRizzle
+        // 'bank' is still set to create Dummy values for testing at this point
+        
+        guard let currentUser = PFUser.current() else{
+            print("No current user")
+            return
+        }
+        
+        let query = PFQuery(className: "Rizzle")
+        query.whereKey("_User", notEqualTo: currentUser.username!)
+        query.findObjectsInBackground { (objects:[PFObject]?, error:Error?) in
+            if error == nil {
+                // find is successful
+                
+            }
+        }
+        
+        self.rizzleTitle.text = self.currentRizzle?.title
+        self.rizzleDescription.text = self.currentRizzle?.question
+        answerCollection.dataSource = self
+        letterBankCollection.dataSource = self.bank
+        self.bank.delegate = self
+        
+    }
+    func enterCellIntoAnswer(_cell: UICollectionViewCell){
+        self.answerArray.add(_cell)
+        self.reloadInputViews()
+        if self.answerArray.count == (self.currentRizzle?.answer?.characters.count)! {
+            let solved = self.checkAnswer()
+            if solved == true{
+                self.rizzleTitle.text = "SOLVED!"
+                self.bank.characterArray.removeAllObjects()
+                // render letterBank and answerArray to unInteractable
+                
+                self.solvedImage.backgroundColor = UIColor.blue
+                self.solvedImage.image = UIImage(named: "solved")
+                self.solvedImage.isHidden = false
+                
+                UIView.animate(withDuration: 1.5, animations: {
+                    self.solvedImage.isHidden = true
+                })
+                
+            }else{
+                // answer field red. possibly offer a hint?
+                self.answerCollection.backgroundColor = UIColor.red
+                self.solvedImage.backgroundColor = UIColor.red
+                self.solvedImage.image = UIImage(named: "incorrect")
+                self.solvedImage.isHidden = false
+                
+                UIView.animate(withDuration: 1.5, animations: {
+                    self.solvedImage.isHidden = true
+                })
+            }
+        }
+        self.reloadInputViews()
+    }
+    func checkAnswer() -> Bool{
+        var solved:Bool = false
+        let toCheck = self.answerArray.componentsJoined(by: "")
+        if self.currentRizzle?.answer == toCheck {
+            solved = true
+        }
+        return solved
+    }
+    
+    @IBAction func returnToDashboard(_ sender: Any) {
+        if let dashboard = self.storyboard?.instantiateViewController(withIdentifier: "userDashboard") as? UserDashboardViewController {
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            appDelegate.window?.rootViewController!.present(dashboard, animated: true, completion: nil)
+        }
+    }
+    
+    //MARK: - UICollectionViewDataSource
+    
+    // select Item to send it back to LetterBank
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.answerCollection.backgroundColor = UIColor.lightGray
+        self.bank.characterArray.add(self.answerArray[indexPath.row])
+        self.answerArray.removeObject(at: indexPath.row)
+    }
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return (self.currentRizzle?.answer?.characters.count)!
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let answerCell = collectionView.dequeueReusableCell(withReuseIdentifier: "answerCell", for: indexPath) as! LetterCell
+        answerCell.configure(image: self.answerArray[indexPath.row] as! String)
+        return answerCell
+    }
+    
+}
