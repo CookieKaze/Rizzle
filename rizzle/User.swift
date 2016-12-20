d7f28df0bff1a46c15929167460bb4a4b712d67b
Matt Mauro <matt.mauro@gmail.com>
2016-12-19 19:47:38 -0500

    coordination

--- rizzle/User.swift
+++ rizzle/User.swift
@@ -24,8 +24,6 @@ class User: NSObject {
     }
     
     
-    
-    
     //MARK: Archiving
     required init(coder decoder: NSCoder) {
         self.username = (decoder.decodeObject(forKey: "username") as? String)!
